dbc2fcf4f4f718b51c6abbe6d92c52983cb9dcea
asconaga <asconaga@gmail.com>
2022-08-26 23:37:10 +0100

    responsive changes

--- client/src/components/footer.js
+++ client/src/components/footer.js
@@ -1,15 +1,12 @@
 import React from 'react';
 
 import { Layout } from 'antd';
-import { useLocation } from 'react-router';
 
 const { Footer } = Layout;
 
 const AppFooter = () => {
-
-    const location = useLocation();
     return (
-        <Footer className="page-footer">Mephisto MAL Client = &lsquo;Developed by lijidea&rsquo; [{location.pathname}]</Footer>
+        <Footer className="page-footer">Mephisto MAL Client = &lsquo;Developed by lijidea&rsquo;</Footer>
     );
 };
 
